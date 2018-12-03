f1543820ee94221dd84adc5f25d5ace36d1624e1
Nikolay Rulev <nikolayr2001@gmail.com>
2018-12-03 20:01:06 +0300

    Very nice commit

--- web/prepare.py
+++ web/prepare.py
@@ -5,6 +5,6 @@ disable all used pins on raspberry starts with Pi startup
 from RPi import GPIO
 from web import Sync, pins
 
-with Sync() as sync:
+with Sync('pins') as sync:
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup([pin for _, pin in pins.items()], GPIO.OUT, initial=GPIO.LOW)
